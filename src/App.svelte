335d337559b7b6492023cee2726a10e9f16230a8
Tommy Bickerdike <tommy@musicglue.com>
2021-01-18 14:07:32 +0000

    begin layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,45 @@
-<script>
-	export let name;
+<script lang="ts">
+	export let name: string;
 </script>
 
 <main>
-	<h1>Machine &amp; Oil is the product design portfolio of {name}</h1>
+	<h1>Machine &amp; Oil is the product design portfolio of <span>{name}</span></h1>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 4rem;
 		margin: 0 auto;
+		min-height: 100vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		font-weight: inherit;
+	}
+	h1 span {
+		display: block;
+		font-size: 6rem;
+		line-height: 1.1;
+		font-weight: 700;
 	}
 
-	@media (min-width: 640px) {
+	aside {
+		display: block;
+		margin-top: -1rem;
+	}
+
+	@media (max-width: 65ch) {
 		main {
-			max-width: none;
+			padding: 1rem;
+			font-size: 13px;
+		}
+		h1 span {
+			font-size: 4rem;
 		}
 	}
 </style>
