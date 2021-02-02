32822ecbb583ad05a7232fdcfddcdcfe487bc982
Tommy Bickerdike <tommy@musicglue.com>
2021-02-02 10:00:32 +0000

    start splitting out

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,42 @@
   import { Router, Route, Link } from "svelte-routing";
   import Intro from "./pages/Intro.svelte";
   import Work from "./pages/Work.svelte";
-  export let url = ""; //This property is necessary declare to avoid ignore the Router
+  export let url = "";
 </script>
 
 <Router url="{url}">
 	<nav>
-		 <Link to="/">Intro</Link>
-		 <Link to="work">Work</Link>
-	 </nav>
-	 <div>
-		 <Route path="work" component="{Work}" /> 
-		 <Route path="/"><Intro /></Route>
-	 </div>
- </Router>
+		<Link to="/">Intro</Link>
+		<Link to="work">Work</Link>
+	</nav>
+	<main>
+		<Route path=""><Intro /></Route>
+		<Route path="work" component="{Work}" /> 
+	</main>
+</Router>
+
+<style>
+	main {
+		padding: 4rem;
+		margin: 0 auto;
+		min-height: 100vh;
+		width: 100%;
+		display: flex;
+		position: relative;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+	}
+
+	nav {
+		position: fixed;
+		top: 0;
+	}
+
+	@media (max-width: 65ch) {
+		main {
+			padding: 1rem;
+			font-size: 13px;
+		}
+	}
+</style>
--- src/pages/Intro.svelte
+++ src/pages/Intro.svelte
@@ -2,30 +2,17 @@
 	import Image from "svelte-image";
 </script>
 
-<main>
-	<div class="main-content">
-		<div class="image">
-			<div class="image-process">
-				<Image src="/tommy-bickerdike.png" />
-			</div>
-		</div>
-		<h1>Machine &amp; Oil is the product design portfolio of <span>Tommy Bickerdike</span></h1>
+
+
+<div class="image">
+	<div class="image-process">
+		<Image src="/tommy-bickerdike.png" />
 	</div>
-</main>
+</div>
+<h1>Machine &amp; Oil is the product design portfolio of <span>Tommy Bickerdike</span></h1>
+
 
 <style>
-	main {
-		padding: 4rem;
-		margin: 0 auto;
-		min-height: 100vh;
-		width: 100%;
-		display: flex;
-		position: relative;
-		flex-direction: column;
-		justify-content: center;
-		align-items: flex-start;
-	}
-
 	h1 {
 		font-weight: inherit;
 		position: relative;
@@ -36,11 +23,6 @@
 		font-size: 6rem;
 		line-height: 1.1;
 		font-weight: 700;
-	}
-
-	aside {
-		display: block;
-		margin-top: -1rem;
 	}
 
 	.image {
@@ -60,11 +42,8 @@
 	.image-process {
 		mix-blend-mode: multiply;
 	}
+
 	@media (max-width: 65ch) {
-		main {
-			padding: 1rem;
-			font-size: 13px;
-		}
 		h1 span {
 			font-size: 4rem;
 		}
