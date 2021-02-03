10b008ec9bf33c152a5d1d7ef5878a8f32538e57
Tommy Bickerdike <tommy@musicglue.com>
2021-02-03 10:09:11 +0000

    refine page transitions

--- src/pages/Intro.svelte
+++ src/pages/Intro.svelte
@@ -1,35 +1,38 @@
 <script lang="ts">
 	import Image from "svelte-image";
-	import { fly } from "svelte/transition";
+	import { fly, fade } from "svelte/transition";
 </script>
 
 <svelte:head>
-  <title>Tommy Bickerdike - Work</title>
+  <title>Tommy Bickerdike - Intro</title>
 </svelte:head>
 
-<section in:fly={{ y: 50, duration: 500 }} out:fly={{ y: 50, duration: 500 }}>
-	<h1>Machine &amp; Oil is the product design portfolio of <span>Tommy Bickerdike</span></h1>
+<section in:fly={{ y: 50, duration: 500 }} out:fly={{ y: 50, duration: 300 }}>
+	<h1>Tommy Bickerdike</h1>
+	<p>A product designer and creative technologist currently 
+		making e-commerce websites as Creative Director of Music Glue.</p>
 </section>
-<div class="image">
+<div class="image" in:fade={{ duration: 2000 }} out:fly={{ x: 100, duration: 300 }}>
 	<div class="image-process">
-		<Image src="/tommy-bickerdike.png" alt="Tommy Bickerdike" width="1500" height="1500" sizes="(max-width: 600px) 80vw, 500px"/>
+		<Image src="/tommy-bickerdike.png" alt="Tommy Bickerdike" width="1500" height="1500" sizes="(max-width: 600px) 80vw, 500px" threshold="0.8"/>
 	</div>
 </div>
 
-
 <style>
-	section {
-		z-index: 2;
-	}
 	h1 {
 		font-weight: inherit;
 		position: relative;
-	}
-	h1 span {
 		display: block;
 		font-size: 6rem;
 		line-height: 1.1;
 		font-weight: 700;
+		margin: 0;
+	}
+
+	p {
+		font-size: 1.5rem;
+		width: 50ch;
+		max-width: 100%;
 	}
 
 	.image {
@@ -51,7 +54,7 @@
 	}
 
 	@media (max-width: 65ch) {
-		h1 span {
+		h1 {
 			font-size: 4rem;
 		}
 	}
--- src/pages/Work.svelte
+++ src/pages/Work.svelte
@@ -7,5 +7,5 @@
 </svelte:head>
 
 <section in:fly={{ y: 50, duration: 500 }} out:fly={{ y: 50, duration: 500 }}>
-  <h1>Work</h1>
+  <h1>Check back soon</h1>
 </section>
