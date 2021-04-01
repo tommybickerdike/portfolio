c1b227fb63acccb0084948415c1277bfb19d2c75
Tommy <tommy@musicglue.com>
2021-04-01 15:09:24 +0100

    index wip

--- src/pages/Work.svelte
+++ src/pages/Work.svelte
@@ -13,22 +13,22 @@
     <a use:link href="/case-studies/ecommerce-sites">
       <dt class="title">Platform UI Design</dt>
       <dd>UI design and development for Music Glue.</dd>
-      <div class="backing-image">
+      <div class="bg">
         <Image src="/images/venndr.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
       </div>
     </a>
     <a use:link href="/case-studies/ecommerce-sites">
       <dt class="title">Branding</dt>
       <dd>Brand logo, supporting styles and website UI. Developed in 2019.</dd>
-      <div class="backing-image">
+      <div class="bg">
         <Image src="/images/music-glue.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
       </div>
     </a>
     <a use:link href="/case-studies/ecommerce-sites">
       <dt class="title">Ecommerce Website Development</dt>
       <dd>A wide range of bespoke e-commerce websites developed for music industry clients.</dd>
-      <div class="backing-image">
-        <Image src="/images/george-ezra.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
+      <div class="bg">
+        <Image src="/images/george-ezra.jpg" width="600" sizes="(max-width: 600px) 80vw,  600px" threshold="0.8"/>
       </div>
     </a>
   </dl>
@@ -41,18 +41,48 @@
   }
   dt {
     font-size: 1.5rem;
-    margin: 0 0 1rem;
     padding: 0;
     display: block;
+    position: relative;
+    z-index: 2;
   }
   dd {
     display: block;
     margin: 0;
     font-weight: normal;
+    position: relative;
+    z-index: 2;
+  }
+  dl { 
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
   }
 
-  a {
-    display: block;
-    margin-bottom: 3rem;
+  .bg {
+      position: relative;
+      width: 100%;
+  }
+
+  @media(hover:hover) {
+    dl a { 
+      position: relative;
+      overflow: hidden;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      min-height: 15rem;
+      padding: 1rem;
+      align-items: center;
+      justify-content: center;
+    }
+    .bg {
+      position: absolute;
+      width: 100%;
+      z-index: -1;
+      height: auto;
+      top: 0;
+      left: 0;
+    }
   }
 </style>
