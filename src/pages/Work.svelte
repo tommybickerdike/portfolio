65de70c0e6c02096c8c0585fd5e4e7e8bd160c95
Tommy <tommy@musicglue.com>
2021-04-08 13:22:53 +0100

    card style work list

--- src/pages/Work.svelte
+++ src/pages/Work.svelte
@@ -11,78 +11,83 @@
 <section in:fade={{ duration: 600 }}>
   <dl>
     <a use:link href="/case-studies/ecommerce-sites">
+      <div class="card-img">
+        <div class="img">
+          <Image src="/images/venndr.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
+        </div>
+      </div>
       <dt class="title">Platform UI Design</dt>
       <dd>UI design and development for Music Glue.</dd>
-      <div class="bg">
-        <Image src="/images/venndr.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
-      </div>
     </a>
     <a use:link href="/case-studies/ecommerce-sites">
+      <div class="card-img">
+        <div class="img">
+          <Image src="/images/music-glue.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
+        </div>
+      </div>
       <dt class="title">Branding</dt>
       <dd>Brand logo, supporting styles and website UI. Developed in 2019.</dd>
-      <div class="bg">
-        <Image src="/images/music-glue.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
-      </div>
     </a>
     <a use:link href="/case-studies/ecommerce-sites">
+      <div class="card-img">
+        <div class="img">
+          <Image src="/images/george-ezra.jpg" width="600" sizes="(max-width: 600px) 80vw,  600px" threshold="0.8"/>
+        </div>
+      </div>
       <dt class="title">Ecommerce Website Development</dt>
       <dd>A wide range of bespoke e-commerce websites developed for music industry clients.</dd>
-      <div class="bg">
-        <Image src="/images/george-ezra.jpg" width="600" sizes="(max-width: 600px) 80vw,  600px" threshold="0.8"/>
-      </div>
     </a>
   </dl>
 </section>
 
 
 <style>
-  section {
-    z-index: 2;
-  }
-  dt {
-    font-size: 1.5rem;
-    padding: 0;
-    display: block;
-    position: relative;
-    z-index: 2;
-  }
-  dd {
-    display: block;
-    margin: 0;
-    font-weight: normal;
-    position: relative;
-    z-index: 2;
-  }
   dl { 
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 2rem;
   }
 
-  .bg {
-      position: relative;
+  a {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--card-color);
+    border-radius: .5rem;
+    overflow: hidden;
+    border: 1px solid var(--border-color);
+    background: var(--card-color);
+  }
+  dt {
+    font-size: 1.5rem;
+    padding: 1rem 1rem 0 1rem;
+    display: block;
+  }
+  dd {
+    display: block;
+    margin: 0;
+    font-weight: normal;
+    font-size: 1rem;
+    padding: .5rem 1rem 1rem 1rem;
+  }
+
+  .card-img {
       width: 100%;
+      height: 0;
+      padding-bottom: 60%;
+      overflow: hidden;
+  }
+
+  .img {
+    transition: transform .2s;
   }
 
   @media(hover:hover) {
-    dl a { 
-      position: relative;
-      overflow: hidden;
-      display: flex;
-      flex-direction: column;
-      text-align: center;
-      min-height: 15rem;
-      padding: 1rem;
-      align-items: center;
-      justify-content: center;
+    a:hover {
+      box-shadow: 0px 2px 4px var(--border-color);
     }
-    .bg {
-      position: absolute;
-      width: 100%;
-      z-index: -1;
-      height: auto;
-      top: 0;
-      left: 0;
+    a:hover .img {
+      transform: scale(1.1);
     }
   }
+
 </style>
