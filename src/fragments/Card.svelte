064961b2f5a8386c27f828234eb6b6bc306ed2e2
Tommy <tommy@musicglue.com>
2021-04-08 16:17:18 +0100

    fragment cards

---
+++ src/fragments/Card.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { link } from "svelte-routing";
+  import Image from "svelte-image";
+  export let href;
+  export let image;
+</script>
+
+<a use:link href={href}>
+  <div class="card-img">
+    <div class="img">
+      <Image src={image} width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
+    </div>
+  </div>
+  <dt><slot name="title"/></dt>
+  <dd><slot name="description"/></dd>
+</a>
+
+<style>
+  a {
+      display: flex;
+      flex-direction: column;
+      background-color: var(--card-color);
+      border-radius: .5rem;
+      overflow: hidden;
+      border: 1px solid var(--border-color);
+      background: var(--card-color);
+  }
+
+  dt {
+    font-size: 1.5rem;
+    padding: 1rem 1rem 0 1rem;
+    display: block;
+  }
+
+  dd {
+    display: block;
+    margin: 0;
+    font-weight: normal;
+    font-size: 1rem;
+    padding: .5rem 1rem 1rem 1rem;
+  }
+
+  .card-img {
+      width: 100%;
+      height: 0;
+      padding-bottom: 60%;
+      overflow: hidden;
+  }
+
+  .img {
+    transition: transform .2s;
+  }
+
+  @media(hover:hover) {
+    a:hover {
+      box-shadow: 0px 2px 4px var(--border-color);
+    }
+    a:hover .img {
+      transform: scale(1.1);
+    }
+  }
+</style>
--- src/pages/Work.svelte
+++ src/pages/Work.svelte
@@ -3,40 +3,23 @@
 </svelte:head>
 
 <script>
-  import Image from "svelte-image";
+  
+  import Card from '../fragments/Card.svelte';
   import { fade } from "svelte/transition";
   import { link } from "svelte-routing";
 </script>
 
 <section in:fade={{ duration: 600 }}>
   <dl>
-    <a use:link href="/case-studies/ecommerce-sites">
-      <div class="card-img">
-        <div class="img">
-          <Image src="/images/venndr.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
-        </div>
-      </div>
-      <dt class="title">Platform UI Design</dt>
-      <dd>UI design and development for Music Glue.</dd>
-    </a>
-    <a use:link href="/case-studies/ecommerce-sites">
-      <div class="card-img">
-        <div class="img">
-          <Image src="/images/music-glue.jpg" width="600" sizes="(max-width: 600px) 80vw, 600px" threshold="0.8"/>
-        </div>
-      </div>
-      <dt class="title">Branding</dt>
-      <dd>Brand logo, supporting styles and website UI. Developed in 2019.</dd>
-    </a>
-    <a use:link href="/case-studies/ecommerce-sites">
-      <div class="card-img">
-        <div class="img">
-          <Image src="/images/george-ezra.jpg" width="600" sizes="(max-width: 600px) 80vw,  600px" threshold="0.8"/>
-        </div>
-      </div>
-      <dt class="title">Ecommerce Website Development</dt>
-      <dd>A wide range of bespoke e-commerce websites developed for music industry clients.</dd>
-    </a>
+    <Card href={"/case-studies/ecommerce-sites"} image={"/images/venndr.jpg"}>
+      <span slot="title">Platform UI Design</span> 
+      <span slot="description">UI design and development for Music Glue.</span>
+    </Card>
+    
+    <Card href={"/case-studies/second"} image={"/images/music-glue.jpg"}>
+      <span slot="title">Platform UI Design</span> 
+      <span slot="description">UI design and development for Music Glue.</span>
+    </Card>
   </dl>
 </section>
 
@@ -48,46 +31,9 @@
     gap: 2rem;
   }
 
-  a {
-    display: flex;
-    flex-direction: column;
-    background-color: var(--card-color);
-    border-radius: .5rem;
-    overflow: hidden;
-    border: 1px solid var(--border-color);
-    background: var(--card-color);
-  }
-  dt {
-    font-size: 1.5rem;
-    padding: 1rem 1rem 0 1rem;
-    display: block;
-  }
-  dd {
-    display: block;
-    margin: 0;
-    font-weight: normal;
-    font-size: 1rem;
-    padding: .5rem 1rem 1rem 1rem;
-  }
-
-  .card-img {
-      width: 100%;
-      height: 0;
-      padding-bottom: 60%;
-      overflow: hidden;
-  }
-
-  .img {
-    transition: transform .2s;
-  }
-
-  @media(hover:hover) {
-    a:hover {
-      box-shadow: 0px 2px 4px var(--border-color);
-    }
-    a:hover .img {
-      transform: scale(1.1);
+  @media(max-width: 650px) {
+    dl {
+      grid-template-columns: repeat(1, 1fr);
     }
   }
-
 </style>
